b50f30a2ce8cb0fa47338e4156b352da3d13e219
Tom-Roman <69944327+tfr8811@users.noreply.github.com>
2025-09-05 15:16:41 -0400

    Undo implementation

--- Scenes/Player/player_grid.gd
+++ Scenes/Player/player_grid.gd
@@ -10,22 +10,33 @@ var mapDimensions = Vector2(9, 6)
 @export var raycastSLong : RayCast2D
 @export var raycastELong : RayCast2D
 @export var raycastWLong : RayCast2D
+var undoRedo = UndoRedo.new()
+var nextStep = Vector2(0, 0)
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	nextStep = Vector2(0, 0)
 	if (Input.is_action_just_pressed("Backward") && !raycastS.is_colliding()):
-		position.y += STEP_SIZE
+		nextStep.y += STEP_SIZE
 	elif (Input.is_action_just_pressed("Forward") && !raycastN.is_colliding()):
-		position.y -= STEP_SIZE
+		nextStep.y -= STEP_SIZE
 	elif (Input.is_action_just_pressed("Left") && !raycastW.is_colliding()):
-		position.x -= STEP_SIZE
+		nextStep.x -= STEP_SIZE
 	elif (Input.is_action_just_pressed("Right") && !raycastE.is_colliding()):
-		position.x += STEP_SIZE
-	if (Input.is_action_just_pressed("WarpSouth") && !raycastSLong.is_colliding()):
-		position.y += STEP_SIZE * mapDimensions.y
+		nextStep.x += STEP_SIZE
+	elif (Input.is_action_just_pressed("WarpSouth") && !raycastSLong.is_colliding()):
+		nextStep.y += STEP_SIZE * mapDimensions.y
 	elif (Input.is_action_just_pressed("WarpNorth") && !raycastNLong.is_colliding()):
-		position.y -= STEP_SIZE * mapDimensions.y
+		nextStep.y -= STEP_SIZE * mapDimensions.y
 	elif (Input.is_action_just_pressed("WarpWest") && !raycastWLong.is_colliding()):
-		position.x -= STEP_SIZE * mapDimensions.x
+		nextStep.x -= STEP_SIZE * mapDimensions.x
 	elif (Input.is_action_just_pressed("WarpEast") && !raycastELong.is_colliding()):
-		position.x += STEP_SIZE * mapDimensions.x
+		nextStep.x += STEP_SIZE * mapDimensions.x
+	elif (Input.is_action_just_pressed("Undo")):
+		undoRedo.undo()
+	if nextStep.length() != 0:
+		undoRedo.create_action("Move Player")
+		undoRedo.add_do_method(MovePlayer.bind(nextStep))
+		undoRedo.add_undo_method(MovePlayer.bind(-nextStep))
+		undoRedo.commit_action()
+
+func MovePlayer(step : Vector2):
+	position += step
